9105c72dce46704abf375f216e813e22786dcc93
Checco <francesco.zaza0510@gmail.com>
2025-04-30 13:04:46 +0200

    modifica db_catalogo

--- db_catalogo.sql
+++ db_catalogo.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Creato il: Apr 30, 2025 alle 11:37
+-- Creato il: Apr 30, 2025 alle 13:04
 -- Versione del server: 10.4.32-MariaDB
--- Versione PHP: 8.0.30
+-- Versione PHP: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -42,16 +42,16 @@ CREATE TABLE `articolo` (
 --
 
 INSERT INTO `articolo` (`id_articolo`, `marchio`, `modello`, `sistema_operativo`, `memoria`, `prezzo`, `immagine`) VALUES
-(1, 'Apple', 'iPhone 14', 'iOS', '128 GB', 999.99, 'no_img.png'),
-(2, 'Samsung', 'Galaxy S23', 'Android', '256 GB', 899.99, 'no_img.png'),
-(3, 'Xiaomi', 'Redmi Note 12', 'Android', '128 GB', 299.99, 'no_img.png'),
-(4, 'Google', 'Pixel 7', 'Android', '128 GB', 649.99, 'no_img.png'),
-(5, 'Huawei', 'P50 Pro', 'HarmonyOS', '256 GB', 799.99, 'no_img.png'),
-(6, 'Sony', 'Xperia 1 V', 'Android', '512 GB', 1199.99, 'no_img.png'),
-(7, 'OnePlus', '11', 'Android', '256 GB', 699.99, 'no_img.png'),
-(8, 'Oppo', 'Find X6', 'Android', '256 GB', 749.99, 'no_img.png'),
-(9, 'Motorola', 'Edge 40', 'Android', '128 GB', 549.99, 'no_img.png'),
-(10, 'Nokia', 'G60', 'Android', '128 GB', 299.99, 'no_img.png');
+(1, 'Apple', 'iPhone 14', 'iOS', '128 GB', 999.99, '/images/iphone14.jpg'),
+(2, 'Samsung', 'Galaxy S23', 'Android', '256 GB', 899.99, '/images/GalaxyS23.jpg'),
+(3, 'Xiaomi', 'Redmi Note 12', 'Android', '128 GB', 299.99, '/images/RedmiNote12.jpg'),
+(4, 'Google', 'Pixel 7', 'Android', '128 GB', 649.99, '/images/Pixel7.jpg'),
+(5, 'Huawei', 'P50 Pro', 'HarmonyOS', '256 GB', 799.99, '/images/P50Pro.jpg'),
+(6, 'Sony', 'Xperia 1 V', 'Android', '512 GB', 1199.99, '/images/Xperia1v.webp'),
+(7, 'OnePlus', '11', 'Android', '256 GB', 699.99, '/images/oneplus11.jpg'),
+(8, 'Oppo', 'Find X6', 'Android', '256 GB', 749.99, '/images/FindX7.jpg'),
+(9, 'Motorola', 'Edge 40', 'Android', '128 GB', 549.99, '/images/Edge40.jpg'),
+(10, 'Nokia', 'G60', 'Android', '128 GB', 299.99, '/images/NokiaG60.jpeg');
 
 --
 -- Indici per le tabelle scaricate
