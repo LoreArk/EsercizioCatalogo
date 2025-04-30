45ded45d87e0c8a70f0e9b612ac669358bdf090d
Gianmarco Minisi <gia.minisi@gmail.com>
2025-04-30 11:27:26 +0200

    Create articolo.sql

---
+++ articolo.sql
@@ -0,0 +1,79 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Creato il: Apr 30, 2025 alle 11:22
+-- Versione del server: 10.4.32-MariaDB
+-- Versione PHP: 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_catalogo`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `articolo`
+--
+
+CREATE TABLE `articolo` (
+  `id_articolo` int(11) NOT NULL,
+  `marchio` varchar(50) NOT NULL,
+  `modello` varchar(50) NOT NULL,
+  `sistema_operativo` varchar(50) NOT NULL,
+  `memoria` varchar(50) NOT NULL,
+  `prezzo` decimal(10,2) NOT NULL,
+  `immagine` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dump dei dati per la tabella `articolo`
+--
+
+INSERT INTO `articolo` (`id_articolo`, `marchio`, `modello`, `sistema_operativo`, `memoria`, `prezzo`, `immagine`) VALUES
+(1, 'Apple', 'iPhone 14', 'iOS', '128 GB', 999.99, 'no_img.png'),
+(2, 'Samsung', 'Galaxy S23', 'Android', '256 GB', 899.99, 'no_img.png'),
+(3, 'Xiaomi', 'Redmi Note 12', 'Android', '128 GB', 299.99, 'no_img.png'),
+(4, 'Google', 'Pixel 7', 'Android', '128 GB', 649.99, 'no_img.png'),
+(5, 'Huawei', 'P50 Pro', 'HarmonyOS', '256 GB', 799.99, 'no_img.png'),
+(6, 'Sony', 'Xperia 1 V', 'Android', '512 GB', 1199.99, 'no_img.png'),
+(7, 'OnePlus', '11', 'Android', '256 GB', 699.99, 'no_img.png'),
+(8, 'Oppo', 'Find X6', 'Android', '256 GB', 749.99, 'no_img.png'),
+(9, 'Motorola', 'Edge 40', 'Android', '128 GB', 549.99, 'no_img.png'),
+(10, 'Nokia', 'G60', 'Android', '128 GB', 299.99, 'no_img.png');
+
+--
+-- Indici per le tabelle scaricate
+--
+
+--
+-- Indici per le tabelle `articolo`
+--
+ALTER TABLE `articolo`
+  ADD PRIMARY KEY (`id_articolo`);
+
+--
+-- AUTO_INCREMENT per le tabelle scaricate
+--
+
+--
+-- AUTO_INCREMENT per la tabella `articolo`
+--
+ALTER TABLE `articolo`
+  MODIFY `id_articolo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
