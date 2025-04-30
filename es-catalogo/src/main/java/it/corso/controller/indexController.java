7e722fe535d5f52ca16964e0232bbc4b9549b41c
marcodldev <marcodelisi2@gmail.com>
2025-04-30 11:58:15 +0200

    + indexController (model,service)

--- es-catalogo/src/main/java/it/corso/controller/indexController.java
+++ es-catalogo/src/main/java/it/corso/controller/indexController.java
@@ -1,15 +1,27 @@
 package it.corso.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import it.corso.model.Articolo;
+import it.corso.service.ArticoloService;
 
 @Controller
 @RequestMapping("/")
 public class indexController {
 
+    @Autowired
+    private ArticoloService articoloService;
+
     @GetMapping
-    public String renderPage(){
+    public String renderPage(Model model){
+        List<Articolo> articoli = articoloService.elencoArticoli();
+        model.addAttribute("articoli", articoli);
         return "index";
     }
 }
