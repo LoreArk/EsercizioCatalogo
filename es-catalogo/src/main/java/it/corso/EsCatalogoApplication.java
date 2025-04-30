a404a9c438c1d6b0e96fef29b2146cccef916c3e
marcodldev <marcodelisi2@gmail.com>
2025-04-30 11:12:07 +0200

    + spring io

---
+++ es-catalogo/src/main/java/it/corso/EsCatalogoApplication.java
@@ -0,0 +1,13 @@
+package it.corso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsCatalogoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsCatalogoApplication.class, args);
+	}
+
+}
