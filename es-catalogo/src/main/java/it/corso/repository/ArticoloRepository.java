0b6d71031e904130d070a9ac5bcb5f29cd6e3877
LorenzoPusateri <lorepusa95@gmail.com>
2025-04-30 11:59:27 +0200

    Package repository + LibroRepository

---
+++ es-catalogo/src/main/java/it/corso/repository/ArticoloRepository.java
@@ -0,0 +1,7 @@
+package it.corso.repository;
+import org.springframework.data.repository.CrudRepository;
+import it.corso.model.Articolo;
+
+public interface ArticoloRepository extends CrudRepository<Articolo, Integer>{
+
+}
